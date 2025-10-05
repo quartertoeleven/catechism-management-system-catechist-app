c341577e7396aaf9b7b5342d5f0d6fd3042a878f
Quarter To Eleven <quartertoeleven2024@outlook.com>
2025-10-05 17:51:31 +0700

    add PWA

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
 <template>
+  <div class="q-pa-md q-gutter-sm" v-if="showInstallBanner">
+    <q-banner position="bottom" inline-actions rounded class="bg-primary text-white">
+      Ấn vào nút bên cạnh để cài đặt ứng dụng
+      <template v-slot:action>
+        <q-btn flat label="Cài đặt" @click="installApp" />
+      </template>
+    </q-banner>
+  </div>
   <router-view />
 </template>
 
 <script setup>
-//
+import { onMounted, ref } from 'vue'
+
+const showInstallBanner = ref(false)
+let deferredPrompt
+
+onMounted(() => {
+  // handle PWA install promt
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault()
+    console.log('jajajajajjajjajaj')
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e
+    // Show the install prompt
+    showInstallBanner.value = true
+  })
+})
+
+const installApp = () => {
+  showInstallBanner.value = false
+  deferredPrompt.prompt()
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt')
+    } else {
+      console.log('User dismissed the A2HS prompt')
+    }
+    deferredPrompt = null
+  })
+}
 </script>
